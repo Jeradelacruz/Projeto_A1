075f86a8e1217d004e0136b5eb773a645e06c0e3
Jeradelacruz <jeradelacruz@gmail>
2023-08-28 17:20:43 -0300

    Exemplo temp simples

---
+++ recipes/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+
+    return render(request,'recipes/home.html', context={
+        'name' : 'Jeronimo'
+
+    })
+
+def sobre(request):
+
+    return HttpResponse('Sobre')
+
+def contato(request):
+
+    return HttpResponse('Contato')
